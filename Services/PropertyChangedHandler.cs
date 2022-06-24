9680352f3c6115426dd54cc109573defb7783e60
TsuyoshiSenpai <valike.ershoff@yandex.ru>
2022-06-24 11:05:24 +0300

    Добавьте файлы проекта.

---
+++ Services/PropertyChangedHandler.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace LSLCurves
+{
+    public class PropertyChangedHandler : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ View/LSLWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace LSLCurves.View
+{
+    /// <summary>
+    /// Логика взаимодействия для LSLWindow.xaml
+    /// </summary>
+    public partial class LSLWindow : Window
+    {
+        public LSLWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
